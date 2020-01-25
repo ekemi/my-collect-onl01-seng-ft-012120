62c50ade5ee120102f90a5f29180f05f6fbecede
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-25 14:25:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,18 @@
 
 def my_collection(collection)
   
+  counter = 1 
   
+  while i < collection.length 
+   
+     yield(collection[i])
   
-end
+     end
+  
+end 
+
+my_collection( collection) do |elemnt|
+  
+           puts "#{elemnt}"
+           
+           end
