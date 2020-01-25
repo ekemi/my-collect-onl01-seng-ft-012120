90c392863bdbe3687d4e90f25a50214b10ad4eb1
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-25 14:54:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@
 def my_collect(array)
   counter = 0
   name_collection = []
-  while i < array.length
+  while counter < array.length
     name_collection.push yield(array[counter])
   
     counter += 1
